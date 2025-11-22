f6ae4ecf96dfa1bd8afa7a539bbfc79a1d8ac233
nicholasferreira1-jpg <nicholasferreira1@gmail.com>
2025-11-21 22:43:46 -0800

    FIxing errors of code

--- frontend/expense-tracker/src/components/Cards/CharAvatar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const CharAvatar = ({ fullName }) => {
-  return <div className={`${width || 'w-12'} ${height || ""} flex items-center justify-center rounded-full text-gray-900 font-medium bg-gray-100 `}>
-    {getInitials(fullName || "")}       
-   </div>; 
-}
-
-export default CharAvatar
